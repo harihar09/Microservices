b31108586ff637c013ba9ae7d69141487dea0ed1
harish5058 <hariharsangale@gmail.com>
2023-08-22 08:54:05 +0530

    microservices

---
+++ OrderService/OrderService/src/main/java/com/microService/OrderService/controller/OrderController.java
@@ -0,0 +1,11 @@
+package com.microService.OrderService.controller;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@Log4j2
+@RequestMapping("/order")
+public class OrderController {
+}
